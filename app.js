53ecc90ef992df3b17e3aeb4c098f7d72db31bd0
Edward <ewdss13@gmail.com>
2018-06-26 19:45:25 +0200

    segunda clase

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ console.log(cliente.tipoCliente());
 */
 //Metodo alternativo
 
+/*
+
 function Cliente(nombre, saldo){
      this.nombre = nombre;
      this.saldo = saldo;
@@ -42,3 +44,52 @@ function Cliente(nombre, saldo){
 const persona = new Cliente('Pedro', 20000);
 
 console.log(persona);
+
+*/
+
+
+
+
+
+//string 
+const nombre1 ='pedro';
+const nombre2 = new String('pedro');
+
+console.log(typeof nombre2);
+console.log(nombre2);
+
+
+//numeros
+
+const numero1 = 20;
+const numero2 = new Number(20);
+
+
+//booleanos
+
+const boolean1 = true;
+const boolean2 = new Boolean(true);
+//funciones
+
+const funcion1 = function(a,b) {
+    return a+b;
+}
+
+const funcion2 = new Function('a','b','return 1 + 2');
+
+//Objetos
+
+const persona1 = {
+    nombre : 'juan'
+}
+
+const persona2 = new Object({nombre: 'juan'});
+
+
+//arreglos
+
+const arreglo1 = [1,2,3,4];
+const arreglo2 = new Array(1,2,3,4);
+
+console.log(arreglo1);
+console.log(arreglo2);
